a0d3c2ea16b03babfb842433e53cd9ed43a15399
ahmadexe <muahmad710@gmail.com>
2024-02-24 14:08:58 +0500

    feat: added models

---
+++ lib/models/auth_data.dart
@@ -0,0 +1,111 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+import 'package:softec_app/models/ratings.dart';
+
+class AuthData {
+  final String email;
+  final String password;
+  final String name;
+  final String uid;
+  final bool isProfessional;
+  final String experties;
+  final String focus;
+  final List<Rating> ratings;
+  
+  AuthData({
+    required this.email,
+    required this.password,
+    required this.name,
+    required this.uid,
+    required this.isProfessional,
+    required this.experties,
+    required this.focus,
+    required this.ratings,
+  });
+
+  AuthData copyWith({
+    String? email,
+    String? password,
+    String? name,
+    String? uid,
+    bool? isProfessional,
+    String? experties,
+    String? focus,
+    List<Rating>? ratings,
+  }) {
+    return AuthData(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      name: name ?? this.name,
+      uid: uid ?? this.uid,
+      isProfessional: isProfessional ?? this.isProfessional,
+      experties: experties ?? this.experties,
+      focus: focus ?? this.focus,
+      ratings: ratings ?? this.ratings,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'password': password,
+      'name': name,
+      'uid': uid,
+      'isProfessional': isProfessional,
+      'experties': experties,
+      'focus': focus,
+      'ratings': ratings.map((x) => x.toMap()).toList(),
+    };
+  }
+
+  factory AuthData.fromMap(Map<String, dynamic> map) {
+    return AuthData(
+      email: map['email'] as String,
+      password: map['password'] as String,
+      name: map['name'] as String,
+      uid: map['uid'] as String,
+      isProfessional: map['isProfessional'] as bool,
+      experties: map['experties'] as String,
+      focus: map['focus'] as String,
+      ratings: List<Rating>.from((map['ratings'] as List<int>).map<Rating>((x) => Rating.fromMap(x as Map<String,dynamic>),),),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory AuthData.fromJson(String source) => AuthData.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'AuthData(email: $email, password: $password, name: $name, uid: $uid, isProfessional: $isProfessional, experties: $experties, focus: $focus, ratings: $ratings)';
+  }
+
+  @override
+  bool operator ==(covariant AuthData other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.email == email &&
+      other.password == password &&
+      other.name == name &&
+      other.uid == uid &&
+      other.isProfessional == isProfessional &&
+      other.experties == experties &&
+      other.focus == focus &&
+      listEquals(other.ratings, ratings);
+  }
+
+  @override
+  int get hashCode {
+    return email.hashCode ^
+      password.hashCode ^
+      name.hashCode ^
+      uid.hashCode ^
+      isProfessional.hashCode ^
+      experties.hashCode ^
+      focus.hashCode ^
+      ratings.hashCode;
+  }
+}
---
+++ lib/models/ratings.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+class Rating {
+  final String name;
+  final int rating;
+  final String review;
+  
+  Rating({
+    required this.name,
+    required this.rating,
+    required this.review,
+  });
+
+  Rating copyWith({
+    String? name,
+    int? rating,
+    String? review,
+  }) {
+    return Rating(
+      name: name ?? this.name,
+      rating: rating ?? this.rating,
+      review: review ?? this.review,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'rating': rating,
+      'review': review,
+    };
+  }
+
+  factory Rating.fromMap(Map<String, dynamic> map) {
+    return Rating(
+      name: map['name'] as String,
+      rating: map['rating'] as int,
+      review: map['review'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Rating.fromJson(String source) => Rating.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Rating(name: $name, rating: $rating, review: $review)';
+
+  @override
+  bool operator ==(covariant Rating other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.name == name &&
+      other.rating == rating &&
+      other.review == review;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ rating.hashCode ^ review.hashCode;
+}
