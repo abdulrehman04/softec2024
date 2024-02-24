b273fc98e51a4ca80b3fbd3e42a70461c1b14a79
ahmadexe <muahmad710@gmail.com>
2024-02-24 19:31:25 +0500

    feat: added analytics provider

---
+++ lib/providers/analytics_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_mlkit_pose_detection/google_mlkit_pose_detection.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AnalyticsProvider with ChangeNotifier {
+  XFile? image;
+  bool picked = false;
+  InputImage? inputImage;
+
+  void pickImage() async {
+    final imagePicker = ImagePicker();
+    image = await imagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+    picked = true;
+    inputImage = InputImage.fromFilePath(image!.path);
+    notifyListeners();
+  }
+
+  void clearImage() {
+    image = null;
+    inputImage = null;
+    picked = false;
+    notifyListeners();
+  }
+}
