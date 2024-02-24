dbbd45a45e971019ebfe5f5343971c3d80f26737
ahmadexe <muahmad710@gmail.com>
2024-02-24 18:05:44 +0500

    feat: search added

--- lib/screens/search_screen/_body.dart
+++ lib/screens/search_screen/_body.dart
@@ -12,6 +12,7 @@ class _BodyState extends State<_Body> {
   void initState() {
     super.initState();
     final authCubit = Provider.of<AuthService>(context, listen: false);
+    authCubit.fetchAllUsers();
     final chatCubit = ChatCubit.c(context);
     final user = authCubit.authData;
     chatCubit.fetchChats(user!.uid);
@@ -19,100 +20,108 @@ class _BodyState extends State<_Body> {
 
   @override
   Widget build(BuildContext context) {
+    final authCubit = Provider.of<AuthService>(context);
+    final chatCubit = ChatCubit.c(context, true);
+    final user = authCubit.authData!;
+
     final screenState = _ScreenState.s(context, true);
 
     List<ChatMeta> list = [];
-    return SafeArea(
-      child: Scaffold(
-        bottomNavigationBar: const BottomBar(),
-        appBar: AppBar(
-          title: const Text('Search'),
-        ),
-        body: Padding(
-          padding: Space.all(),
-          child: BlocBuilder<ChatCubit, ChatState>(
-            builder: (context, state) {
-              if (state.fetchChats is FetchChatsLoading) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
+    return Scaffold(
+      bottomNavigationBar: const BottomBar(),
+      body: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Search'),
+          ),
+          body: Padding(
+            padding: Space.all(),
+            child: BlocBuilder<ChatCubit, ChatState>(
+              builder: (context, state) {
+                if (state.fetchChats is FetchChatsLoading ||
+                    authCubit.allUsers == null) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
 
-              if (state.fetchChats is FetchChatsSuccess) {
-                list = screenState.hasSearched
-                    ? screenState.filtered
-                    : state.fetchChats.data ?? [];
+                if (state.fetchChats is FetchChatsSuccess) {
+                  list = screenState.hasSearched
+                      ? screenState.filtered
+                      : state.fetchChats.data ?? [];
 
-                if (list.isEmpty && !screenState.hasSearched) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(
-                        Iconsax.messages,
-                        size: 15,
-                        color: Colors.grey,
-                      ),
-                      Space.y2!,
-                      Padding(
-                        padding: Space.all(),
-                        child: Text(
-                          'To start messaging people who have Hotspot, tap the button below and get started.',
-                          style: AppText.b2!.cl(AppTheme.c.primary!),
-                          textAlign: TextAlign.center,
+                  if (list.isEmpty && !screenState.hasSearched) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(
+                          Iconsax.messages,
+                          size: 15,
+                          color: Colors.grey,
                         ),
+                        Space.y2!,
+                        Padding(
+                          padding: Space.all(),
+                          child: Text(
+                            'To start messaging people who have Hotspot, tap the button below and get started.',
+                            style: AppText.b2!.cl(AppTheme.c.primary!),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Space.y2!,
+                        AppButton(
+                          width: 150,
+                          label: 'Get Started',
+                          onPressed: () {
+                            AppRouter.push(context, const AllUsersScreen());
+                          },
+                        ),
+                      ],
+                    );
+                  }
+
+                  return Column(
+                    children: [
+                      AppTextField(
+                        name: 'search',
+                        prefixIcon: const Icon(
+                          Iconsax.search_normal_1,
+                        ),
+                        hint: 'Search...',
+                        onChanged: (query) {
+                          if (query != null) {
+                            screenState.search(query, list);
+                          }
+                        },
                       ),
                       Space.y2!,
-                      AppButton(
-                        width: 150,
-                        label: 'Get Started',
-                        onPressed: () {
-                          AppRouter.push(context, const AllUsersScreen());
-                        },
+                      Expanded(
+                        child: ListView.builder(
+                          padding: Space.z,
+                          itemCount: list.length,
+                          itemBuilder: (context, index) {
+                            final chat = list[index];
+
+                            return ListTile(
+                              leading: const Icon(Icons.person),
+                              title: Text(chat.receiver.fullname),
+                              onTap: () {},
+                            );
+                          },
+                        ),
                       ),
                     ],
                   );
                 }
 
-                return Column(
-                  children: [
-                    AppTextField(
-                      name: 'search',
-                      prefixIcon: const Icon(
-                        Iconsax.search_normal_1,
-                      ),
-                      hint: 'Search...',
-                      onChanged: (query) {
-                        if (query != null) {
-                          screenState.search(query, list);
-                        }
-                      },
-                    ),
-                    Space.y2!,
-                    Expanded(
-                      child: ListView.builder(
-                        padding: Space.z,
-                        itemCount: list.length,
-                        itemBuilder: (context, index) {
-                          final chat = list[index];
-
-                          return ListTile(
-                            title: Text(chat.receiver.fullname),
-                            onTap: () {},
-                          );
-                        },
-                      ),
-                    ),
-                  ],
+                return Center(
+                  child: Text(
+                    "Looks Like an error occured, please try again later",
+                    style: AppText.b2!.cl(AppTheme.c.primary!),
+                  ),
                 );
-              }
-
-              return Center(
-                child: Text(
-                  "Looks Like an error occured, please try again later",
-                  style: AppText.b2!.cl(AppTheme.c.primary!),
-                ),
-              );
-            },
+              },
+            ),
           ),
         ),
       ),
